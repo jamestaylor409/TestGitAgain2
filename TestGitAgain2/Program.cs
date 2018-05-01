706922db31187831a2ecabae41e1f4c9e5c32953
James Taylor <jamestaylor409@gmail.com>
2018-05-01 12:02:05 -0600

    testing agian

--- TestGitAgain2/Program.cs
+++ TestGitAgain2/Program.cs
@@ -7,6 +7,7 @@ namespace TestGitAgain2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Trying to save!");
         }
     }
 }
