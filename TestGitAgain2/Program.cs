338b4bc850c7829a5119eb0f0afe5865b95a7169
James Taylor <jamestaylor409@gmail.com>
2018-05-01 11:54:14 -0600

    Add project files.

---
+++ TestGitAgain2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestGitAgain2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
